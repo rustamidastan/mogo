914c838a78eb3377023d85b4843be83a9c41a894
rustami-dastan <rustami.dastan0414@gmail.com>
2021-06-24 18:18:24 +0500

    second commit

--- js/main.js
+++ js/main.js
@@ -7,3 +7,18 @@ menuIcon.addEventListener('click', function () {
         siteNav.style.display = 'none'
     else siteNav.style.display = 'flex'
 })
+
+
+// START WE DO TABS 
+let weDoButton = document.querySelectorAll('.we-do__tab-btn');
+let weDoText = document.querySelectorAll('.we-do__tab-text')
+let weDoTab = document.querySelectorAll('.we-do-tab');
+
+for (let i = 0; i < 3; i++) {
+    weDoButton[i].addEventListener('click', function () {
+        weDoTab[0].classList.remove('we-do__tab-active');
+        weDoTab[1].classList.remove('we-do__tab-active');
+        weDoTab[2].classList.remove('we-do__tab-active');
+        weDoTab[i].classList.toggle('we-do__tab-active');
+    })
+}
