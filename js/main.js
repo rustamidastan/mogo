113b12a1c88eeee05fc846d653211beda40ae5e9
rustamidastan <rustami.dastan0414@gmail.com>
2022-09-08 09:16:57 +0500

    changing hamburger menu features

--- js/main.js
+++ js/main.js
@@ -1,15 +1,21 @@
-// SITE HEADER HAMBURGER MENU 
+// SITE HEADER HAMBURGER MENU
 
-let menuIcon = document.querySelector('.site-header__hamburger-icon');
-let siteNav = document.querySelector('.sitenav');
-menuIcon.addEventListener('click', function () {
-    if (siteNav.style.display == 'flex')
-        siteNav.style.display = 'none'
-    else siteNav.style.display = 'flex'
-})
+let menuIcon = document.querySelector(".site-header__hamburger-icon");
+let siteNav = document.querySelector(".sitenav");
+let siteNavLinks = document.querySelectorAll(".sitenav__link");
+menuIcon.addEventListener("click", () => {
+  siteNav.classList.toggle("sitenav-active");
+  document.body.classList.toggle("body-active");
+});
 
+siteNavLinks.forEach((item) => {
+  item.addEventListener("click", () => {
+    siteNav.classList.remove("sitenav-active");
+    document.body.classList.remove("body-active");
+  });
+});
 
-// START WE DO TABS 
+// START WE DO TABS
 // let weDoButton = document.querySelectorAll('.we-do__tab-btn');
 // let weDoText = document.querySelectorAll('.we-do__tab-text')
 // let weDoTab = document.querySelectorAll('.we-do-tab');
@@ -26,52 +32,46 @@ menuIcon.addEventListener('click', function () {
 const questions = document.querySelectorAll(".we-do-tab");
 
 questions.forEach(function (question) {
-    const btn = question.querySelector(".we-do__tab-btn");
+  const btn = question.querySelector(".we-do__tab-btn");
 
-    btn.addEventListener("click", function () {
-
-        questions.forEach(function (item) {
-            if (item !== question) {
-                item.classList.remove("we-do__tab-active");
-            }
-        });
-
-        question.classList.toggle("we-do__tab-active");
+  btn.addEventListener("click", function () {
+    questions.forEach(function (item) {
+      if (item !== question) {
+        item.classList.remove("we-do__tab-active");
+      }
     });
+
+    question.classList.toggle("we-do__tab-active");
+  });
 });
 
+let searchBtn = document.querySelector(".site-header__search-btn");
+let inputSearch = document.querySelector(".serach-input");
 
-let searchBtn = document.querySelector('.site-header__search-btn');
-let inputSearch = document.querySelector('.serach-input');
+searchBtn.addEventListener("click", function () {
+  if (inputSearch.style.display == "block") inputSearch.style.display = "none";
+  else inputSearch.style.display = "block";
+});
 
-searchBtn.addEventListener('click', function () {
-    if (inputSearch.style.display == 'block')
-        inputSearch.style.display = 'none';
-    else inputSearch.style.display = 'block'
-})
+let dotsItem = document.querySelector(".slick-dots");
 
-
-
-let dotsItem = document.querySelector('.slick-dots')
-
-$('.site__hero-carousel').slick({
-    dots: true,
-    customPaging: function (slider, i) {
-        if (i == 0) {
-            return '<span class="slider_num">01</span> intro';
-
-        } else if (i == 1) {
-            return '<span class="slider_num">02</span> work';
-        } else if (i == 2) {
-            return '<span class="slider_num">03</span> about';
-        } else if (i == 3) {
-            return '<span class="slider_num">04</span> contact';
-        }
-    },
-    infinite: true,
-    speed: 1000,
-    slidesToShow: 1,
-    autoplay: true,
-    autoplaySpeed: 1000,
-    arrows: false
+$(".site__hero-carousel").slick({
+  dots: true,
+  customPaging: function (slider, i) {
+    if (i == 0) {
+      return '<span class="slider_num">01</span> intro';
+    } else if (i == 1) {
+      return '<span class="slider_num">02</span> work';
+    } else if (i == 2) {
+      return '<span class="slider_num">03</span> about';
+    } else if (i == 3) {
+      return '<span class="slider_num">04</span> contact';
+    }
+  },
+  infinite: true,
+  speed: 1000,
+  slidesToShow: 1,
+  autoplay: true,
+  autoplaySpeed: 1000,
+  arrows: false,
 });
